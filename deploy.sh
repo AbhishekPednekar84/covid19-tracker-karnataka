98825008e14ea27ea3a47446be7c5562f856cd0a
Abhishek Pednekar <abhi_ap@hotmail.com>
2020-06-03 16:41:29 +0530

    Changed deployment directory in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@165.22.215.143:/home/abhiap/covid19-tracker-karnataka-copy/client
-  rsync -a --exclude={'/node_modules'} server/ travis@165.22.215.143:/home/abhiap/covid19-tracker-karnataka-copy/server
+  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@165.22.215.143:/home/abhiap/covid19-tracker-karnataka/client
+  rsync -a --exclude={'/node_modules'} server/ travis@165.22.215.143:/home/abhiap/covid19-tracker-karnataka/server
 else
   echo "Not deploying, since the branch isn't master."
 fi
